a3d11682a026d4e8500298191d39490ce80413c1
Sakibur-coder <sakiburrahman213133@gmail.com>
2024-11-06 00:05:38 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['*'],
+  theme: {
+    screens: {
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      screens:{
+      }
+    },
+  },
+  plugins: [],
+}
+
